2ef0e1d2d15598686b3216fa22a80506d1bdb5f4
Robert Schmoltzi <uhwke@student.kit.edu>
2020-04-16 16:57:31 +0200

    Updated gitignore. Test script for ifc.

---
+++ scripts/test.py
@@ -0,0 +1,52 @@
+import subprocess
+
+console = "../cmake-build-debug/console"
+
+path = "../affinity/"
+graph_name = "fe_ocean"
+graph_ext = ".graph"
+graph_path = path + graph_name + graph_ext
+
+ord_type = "-aff"
+ord_ext = ".ord"
+ord_path = path + graph_name + ord_type + ord_ext
+
+def test_ifc():
+    args = [console]
+
+    args.append("load_metis_graph")
+    args.append(graph_path)
+
+    args.append("load_node_orders")
+    args.append(ord_path)
+
+    args.append("add_back_arcs")
+    args.append("remove_multi_arcs")
+    args.append("remove_loops")
+
+    args.append("flow_cutter_set")
+    args.append("random_seed")
+    args.append("5489")
+
+    args.append("flow_cutter_set")
+    args.append("ReportCuts")
+    args.append("no")
+
+    args.append("flow_cutter_set")
+    args.append("max_cut_size")
+    args.append("4000") # default:1000 m14b:4000
+
+    args.append("flow_cutter_accelerated_enum_cuts_from_orders")
+    args.append("-")
+
+    run(args)
+
+def run(args):
+    output = subprocess.check_output(args, universal_newlines=True)
+    print(output)
+    # if output.returncode == 0:
+    #     print(output.output)
+    # else:
+    #     print("Error, console crashed")
+
+test_ifc()
