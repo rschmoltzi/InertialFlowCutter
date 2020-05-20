85594ef14aab4d05d46ea61a025a251b502556c1
Robert Schmoltzi <uhwke@student.kit.edu>
2020-05-20 12:55:50 +0200

    Added comments to some methods

--- scripts/affinity_order.py
+++ scripts/affinity_order.py
@@ -21,6 +21,10 @@ def calculate_and_save_order(path_to_graph, path_to_ord, ordering_alg, amount_or
         f.write(config.DELIMITER_ORDER.join(config.DELIMITER_NODE.join(str(i) for i in order) for order in orders))
 
 def load_graph(path_to_graph, reader=nk.graphio.METISGraphReader()):
+    '''
+    Loads the graph specified by the path. For graph formats other than METIS a fiiting networkit reader must be supplied.
+    '''
+    
     if config.TIME_STAMPS >= config.TimeStamps.ALL:
         before = pd.Timestamp.now()
 
@@ -36,8 +40,15 @@ def load_graph(path_to_graph, reader=nk.graphio.METISGraphReader()):
 def random_reorder(l):
     return random.sample(l, len(l))
 
+def id(l):
+    return l
+
 # Returns a function that realizes a randomized-connected reordering on the given graph g
 def connected_random_reorder_func(g):
+    '''
+    Wrapper function used to put g in the namespace of the returned functino without changing its signature.
+    Returns a function that pseudo randomly reorders a list of clusters based on their connectivity in g.
+    '''
 
     def connected_random_reorder(l):
         if len(l) <= 1:
@@ -47,8 +58,13 @@ def connected_random_reorder_func(g):
         for cluster in l:
             nodes[cluster] = set([node_id for node in PreOrderIter(cluster) if node.is_leaf for node_id in node.old_ids])
 
+        # inverse map
+        # for c in nodes:
+        #     for u in
+
         #choose random cluster c
         c = random.choice(l)
+        # ordered dict
         added_clusters = list()
         added_clusters.append(c)
 
@@ -60,7 +76,7 @@ def connected_random_reorder_func(g):
 
             for u in nodes[c]:
                 for n in g.neighbors(u):
-                    # should run in O(1)
+                    # Tidy this up with inverse map
                     # finds the cluster that n is in and incerments its edge count
                     for cluster in l:
                         if cluster in added_clusters: # assumes that len(l) is small
@@ -81,14 +97,19 @@ def connected_random_reorder_func(g):
 
 
 
-def recursive_PLM_orderings(g, amount_orders):
+def recursive_PLM_orderings(g, amount_orderings):
+    '''
+    Takes a graph and an amount_orderings. Returns a list of orderings based on recursive application of PLM.
+    This list has the length amount_orderings.
+    '''
+
     if config.TIME_STAMPS >= config.TimeStamps.ALL:
         i = 0
         before = pd.Timestamp.now()
 
     root = recursive_PLM(g)
 
-    orderings = get_orderings(root, amount_orders, connected_random_reorder_func(g))
+    orderings = get_orderings(root, amount_orderings, connected_random_reorder_func(g))
 
     if config.TIME_STAMPS >= config.TimeStamps.ALL:
         after = pd.Timestamp.now()
@@ -109,6 +130,10 @@ def subgraph(g, s):
 
 # For the creation of additional orderings one should rewrite the PreOrderIter function
 def recursive_PLM(g):
+    '''
+    Recursively applies PLM to a graph until the partions are not greater than |g| * config.PLM_RESOLUTION.
+    Returns the contraction tree. The leaves carry the old ids.
+    '''
 
     if config.TIME_STAMPS >= config.TimeStamps.ALL:
         i = 0
@@ -138,7 +163,7 @@ def recursive_PLM(g):
 
     return root
 
-def get_ordering_PLM(root, reorder):
+def get_ordering(root, reorder):
     # Slightly modified from https://github.com/c0fec0de/anytree/blob/master/anytree/iterators/preorderiter.py
     class RandomDFSIter(AbstractIter):
 
@@ -160,6 +185,13 @@ def get_ordering_PLM(root, reorder):
     return ordering
 
 def get_orderings(root, n, reorder=None, reorders=None):
+    '''
+    Generates multiple DFS based pseudo random orderings from the given contraction tree. Ordering[x] == y means that
+    node y has the position x in the ordering. The pseudo randomness is based on config.SEED. This method
+    uses the reorder argument to reorder the evaluation of all orderings, if given.
+    Otherwise the reorders argument is used to supply a reorder method for each ordering.
+    '''
+
     if config.TIME_STAMPS >= config.TimeStamps.ALL:
         before = pd.Timestamp.now()
 
@@ -171,7 +203,7 @@ def get_orderings(root, n, reorder=None, reorders=None):
     def get_orderings_from_tree(root, reorders):
         ret = list()
         for i in range(n):
-            ret.append(get_ordering_PLM(root, reorders[i]))
+            ret.append(get_ordering(root, reorders[i]))
 
         return ret
 
@@ -331,6 +363,10 @@ def contract_to_nodes(g, disjoint_set, d):
     return contracted_g
 
 def affinity_tree(g):
+    '''
+    Creates a contraction tree as seen in Aydin, K.; Bateni, M.H.; Mirrokni, V.: Distributed Balanced Partitioning via Linear Embedding.
+
+    '''
     if config.TIME_STAMPS >= config.TimeStamps.ALL:
         i = 0
     set_weights_graph(g)
@@ -366,14 +402,19 @@ def affinity_tree(g):
 
     return last_iteration[0]
 
-def affinity_orderings(g, amount_orders):
+def affinity_orderings(g, amount_orderings):
+    '''
+    Takes a graph and an amount_orderings. Returns a list of orderings based on the affinity contraction tree.
+    This list has the length amount_orderings.
+    '''
+
     if config.TIME_STAMPS >= config.TimeStamps.ALL:
         i = 0
         before = pd.Timestamp.now()
 
     root = affinity_tree(g)
 
-    orderings = get_orderings(root, amount_orders, connected_random_reorder_func(g))
+    orderings = get_orderings(root, amount_orderings, connected_random_reorder_func(g))
 
     if config.TIME_STAMPS >= config.TimeStamps.ALL:
         after = pd.Timestamp.now()
@@ -381,9 +422,14 @@ def affinity_orderings(g, amount_orders):
 
     return orderings
 
+# Stuck at the end because of parse order...
 ORD_ALG = dict(zip(config.ORD_TYPE, [recursive_PLM_orderings, affinity_orderings]))
 
 def main():
+    '''
+    Quick test method.
+    '''
+
     g = load_graph("../affinity/walshaw/uk.graph", nk.graphio.METISGraphReader())
 
     # Test to check for not connected graphs. Remove for more performance
